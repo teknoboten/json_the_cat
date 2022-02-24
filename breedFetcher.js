6aa47977f636a6c885c0e0de5fda255d41870086
modemgirl <serra@modemgirl.ca>
2022-02-24 22:23:27 +0000

    refactor to use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,51 @@
 const request = require('request');
 
-const catName = process.argv[2].toLowerCase();
-const req = `https://api.thecatapi.com/v1/breeds/search?name=${catName}`;
+// const catName = process.argv[2].toLowerCase();
+
+const fetchBreedDescription = (breedName, callback) => {
+  // const req = `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`;
+  const req = `https://api.thecatapii.com/v1/breeds/search?name=${breedName}`; //badreq
+
+  request(req, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+    }
+
+    if (!error) {
+    
+      if (body === '[]') {
+        callback(null, null);
+      } else {
+        const result = JSON.parse(body);
+        const description = result[0]['description'];
+        callback(null, description);
+      }
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
 
 
 
-request(req, (error, response, body) => {
+// request(req, (error, response, body) => {
 
-  if (error) {
-    console.log(`sorry, your request failed. 😿\n`);
-    console.log(error);
-    return;
-  }
+//   if (error) {
+//     console.log(`sorry, your request failed. 😿\n`);
+//     console.log(error);
+//     return;
+//   }
 
-  if (body === "[]") {
-    console.log(`sorry, we couldn't find ${catName}, please try again.`);
-    return;
-  }
+//   if (body === "[]") {
+//     console.log(`sorry, we couldn't find ${catName}, please try again.`);
+//     return;
+//   }
 
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
+//   const data = JSON.parse(body);
+//   console.log(data[0]['description']);
 
-});
+// });
+
+// console.log(data[0]['description']);
---
+++ index.js
@@ -0,0 +1,23 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2].toLowerCase();
+
+fetchBreedDescription(breedName, (error, description) => {
+
+  // console.log(`error: ${error}`);
+  // console.log(`description: ${description}`);
+
+  if (error && !description){  //works for bad url
+    console.log(`sorry, your request failed. 😿\n`);
+    console.log(error);
+  };
+
+  if (!error && !description){  //works for bad url
+    console.log(`sorry, we could not find a match for ${breedName}. 😿\n`);
+  } 
+  
+  if (!error && description){
+    console.log(description);
+  }
+
+});
