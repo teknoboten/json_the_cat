9d0376334cb276bcc14f5f4d2181c4bea45fae5c
modemgirl <serra@modemgirl.ca>
2022-02-24 00:58:21 +0000

    add error handling for failed request and breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,24 @@
 const request = require('request');
 
-const catUrl = process.argv[2];
-// const siberian = 'https://api.thecatapi.com/v1/breeds/search?q=sib'
+const catName = process.argv[2].toLowerCase();
+const req = `https://api.thecatapi.com/v1/breeds/search?name=${catName}`;
 
-console.log(catUrl);
 
-request(catUrl, (error, response, body) => {
-const data = JSON.parse(body);
 
-console.log(data[0]['description']);
+request(req, (error, response, body) => {
+
+  if (error) {
+    console.log(`sorry, your request failed. 😿\n`);
+    console.log(error);
+    return;
+  }
+
+  if (body === "[]") {
+    console.log(`sorry, we couldn't find ${catName}, please try again.`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  console.log(data[0]['description']);
 
 });
